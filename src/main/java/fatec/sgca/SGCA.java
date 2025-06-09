a6d7cece33944a53b0cc8247b114d32bb3a51a40
Wesley Xavier <wesley.xvier@gmail.com>
2025-06-09 10:50:46 -0300

    :bulb: docs: edição de comentários.

--- src/main/java/fatec/sgca/SGCA.java
+++ src/main/java/fatec/sgca/SGCA.java
@@ -1,13 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package fatec.sgca;
 
-/**
- *
- * @author Fatec
- */
 public class SGCA {
 
     public static void main(String[] args) {
